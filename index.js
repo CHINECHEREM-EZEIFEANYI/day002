4cc58a9f385d1e9496e5a174c593051db6cb63fe
CHINECHEREM-EZEIFEANYI <82101590+CHINECHEREM-EZEIFEANYI@users.noreply.github.com>
2022-03-23 23:39:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//declaration of variable
+var x;
+var y;
+
+//use of nested loop 
+for (var x = 1; x <= 10; x++) 
+{
+for (var y = 1; y <= 10; y++) {
+  console.log(`${x} * ${y} =  ${x*y}` ) //template literals
+}
+  console.log("\n")//line break after every successful loop
+  }
+
+
+
+
+
